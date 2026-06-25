ae2ee199391a43d8a8654058db93da80da963c7a
Isaiah Roque <165978474+ii-seya@users.noreply.github.com>
2026-06-25 00:12:52 -0700

    Update main.py
    testing JSON and .txt file reading and error handling

--- main.py
+++ main.py
@@ -1,7 +1,37 @@
 """
-Start here:
-Open JSON file
-Open job desc file (.txt)
+Workflow:
 
-Print contents here
+open and parse json file
+
+open and read .txt file (job desc)
+
+check for:
+1. missing .txt file
+2. malformed JSON file
+
+print results
 """
+import json
+
+# JSON file handling
+try:
+  with open('data.json', 'r') as file:
+    data = json.load(file)
+  print(json.dumps(data, indent=4))
+  
+except json.JSONDecodeError:
+    print("Error: Failed to decode JSON from the file.")
+
+# Job Desc. file handling
+try:
+  with open("jobdesc.txt") as f:
+  print(f.read())
+
+except FileNotFoundError: 
+  print("File does not exist")
+
+finally:
+  f.close()
+  
+  
+
