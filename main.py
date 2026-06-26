1f5f8aca5ad8a65e5b1719fd0086f5098f485638
Isaiah Roque <165978474+ii-seya@users.noreply.github.com>
2026-06-25 18:18:10 -0700

    function test
    fixing "none" error on print statement

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ import json
 def load_resume(filename):
   try:
     with open(filename, 'r') as file:
-      data = json.load(file)
-      print(json.dumps(data, indent=4))
+      resume_data = json.load(file)
+    return resume_data
     
   except json.JSONDecodeError:
       print("Error: Failed to decode JSON from the file.")
@@ -14,8 +14,9 @@ def load_resume(filename):
 def load_job_desc(filename):
   try:
     with open(filename) as file:
-      print(file.read())
-  
+      job_desc_data = file.read()
+    return job_desc_data
+    
   except FileNotFoundError: 
     print("File does not exist")
 
